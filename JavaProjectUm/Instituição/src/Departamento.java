48bab4d85c06ce38411403d54f083224ea023b66
Lucasrc22 <lucasrios2210@gmail.com>
2023-05-15 19:34:53 -0300

    add

--- JavaProjectUm/Instituição/src/Departamento.java
+++ JavaProjectUm/Instituição/src/Departamento.java
@@ -1,13 +1,21 @@
 public class Departamento {
     private String nome;
-    Departamento(String nome){
+    private Area area;
+    Departamento(String nome, Area area){
         this.nome = nome;
+        this.area = area;
     }
     String getNome(){
         return this.nome;
     }
     void setNome(String nome){
         this.nome = nome;
+    }
+    public Area getArea() {
+        return area;
+    }
+    public void setArea(Area area) {
+        this.area = area;
     }
     
 }
--- JavaProjectUm/Instituição/src/Programa.java
+++ JavaProjectUm/Instituição/src/Programa.java
@@ -1,25 +1,23 @@
 public class Programa {
-private static final Departamento deptoComp = new Departamento("Depto de Computação");
-private static final Departamento deptoEnf = new Departamento("Depto de Enfermagem");
-private static final Departamento deptoExatas = new Departamento("Departamento de Química ");
+private static final Departamento deptoComp = new Departamento("Depto de Computação", Area.EXATAS);
+private static final Departamento deptoEnf = new Departamento("Depto de Enfermagem", Area.BIOLOGICAS);
+
     public static void main(String[] args) {
 
         Curso cursoCc = new Curso("Eng. Computação", "Ecomp", deptoComp);
         Curso cursoAds = new Curso("Analise e Desenvolvimento de Sistemas", "ADS", deptoComp);
         Curso cursoEnf = new Curso("Enfermagem", "Enf", deptoEnf);
-        Curso cursoQuim = new Curso("Química", "Quim", deptoExatas);
+
         Professor professorJoao = new Professor("João Silva", "01417176458", deptoComp);
-        Professor professorJandyson = new Professor("Jandyson da Silva", "02252360445", deptoExatas);
         Coordenador coordenador = new Coordenador("Carlos Silva", "70719240158", deptoComp);
         coordenador.addCursoCoord(cursoAds);
         coordenador.addCursoCoord(cursoCc);
-        coordenador.addCursoCoord(cursoEnf);
-        coordenador.addCursoCoord(cursoQuim);
+    
 
         Alocacao alocacao = new Alocacao(professorJoao, cursoCc, null, 0);
-        Alocacao alocacao2 = new Alocacao(professorJandyson,cursoQuim, null, 0);
+        
         System.out.println(alocacao);
-        System.out.println(alocacao2);
+        
 
     }
     
