ff0ed2982ed2f6f3885f79caf82a766004d789b7
Lucasrc22 <lucasrios2210@gmail.com>
2023-05-16 21:49:33 -0300

    add NegocioCurso

--- JavaProject/Instituição/src/modeloPackage/negocioPackage/NegocioCurso.java
+++ JavaProject/Instituição/src/modeloPackage/negocioPackage/NegocioCurso.java
@@ -1,5 +1,46 @@
 package negocioPackage;
+import java.util.ArrayList;
 
+import modeloPackage.Curso;
+import modeloPackage.NegocioException;
+import repositorioPackage.RepositorioCurso;
 public class NegocioCurso {
-    
+    private RepositorioCurso repositorio;
+    private static NegocioCurso instancia;
+
+    public static NegocioCurso getInstancia(){
+        if(instancia == null){
+            instancia = new NegocioCurso();
+        }
+        return instancia;
+    }
+
+    private NegocioCurso(){
+        repositorio = new RepositorioCurso();
+    }
+
+    public void inserirItem(Curso item) throws NegocioException{
+        validarCurso(item);
+
+        repositorio.inserirItem(item);
+    }
+
+    private void validarCurso(Curso item) throws NegocioException{
+        ValidacaoNome.validarNome(item.getNome());
+
+        ArrayList<Curso> cursosDoMesmoDepto = repositorio.consultarDepartamento(item.getDepartamento());
+
+        for(Curso curso: cursosDoMesmoDepto){
+            if(curso.getNome().equals(item.getNome())){
+                throw new NegocioException("Curso com o mesmo nome dá existe no departamento");
+            }
+        }
+
+        if(NegocioIntemComSigla.getInstancia().consultarPorSigla(item.getSigla()) != null){
+            throw new NegocioException("Outro item com a mesma sigla já existe");
+        }
+    }
+    public void removeItem(Curso item){
+        repositorio.removerItem(item);
+    }
 }
